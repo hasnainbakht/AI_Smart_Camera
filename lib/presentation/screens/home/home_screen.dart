0e87d683745999741881ddddb7654db47ffc1a1a
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-11-30 19:01:24 +0500

    In progress 30%

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,27 @@
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Smart Camera")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () => context.go('/camera'),
+            child: const Text("Open Camera"),
+          ),
+          ElevatedButton(
+            onPressed: () => context.go('/gallery'),
+            child: const Text("Gallery"),
+          ),
+          ElevatedButton(
+            onPressed: () => context.go('/settings'),
+            child: const Text("Settings"),
+          ),
+        ],
+      ),
+    );
+  }
+}
