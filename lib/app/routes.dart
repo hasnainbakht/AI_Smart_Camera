736c69732ac509759adc9fac965072b9803647a8
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-11-30 19:09:49 +0500

    In progress 30%

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,4 +1,14 @@
 import 'package:go_router/go_router.dart';
+import '../presentation/screens/splash/splash_screen.dart';
+import '../presentation/screens/permissions/permission_screen.dart';
+import '../presentation/screens/home/home_screen.dart';
+import '../presentation/screens/feedback/feedback_screen.dart';
+import '../presentation/screens/camera/camera_screen.dart';
+import '../presentation/screens/gallery/gallery_screen.dart';
+import '../presentation/screens/settings/settings_screen.dart';
+
+
+
 
 final appRouter = GoRouter(
   initialLocation: '/splash',
@@ -7,7 +17,7 @@ final appRouter = GoRouter(
     GoRoute(path: '/permissions', builder: (_, __) => const PermissionScreen()),
     GoRoute(path: '/home', builder: (_, __) => const HomeScreen()),
     GoRoute(path: '/camera', builder: (_, __) => const CameraScreen()),
-    GoRoute(path: '/feedback', builder: (_, __) => const FeedbackScreen()),
+    GoRoute(path: '/feedback', builder: (_, __) => const FeedbackScreen(imagePath: '',)),
     GoRoute(path: '/gallery', builder: (_, __) => const GalleryScreen()),
     GoRoute(path: '/settings', builder: (_, __) => const SettingsScreen()),
   ],
