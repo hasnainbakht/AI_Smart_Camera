9ac2d1b9c88cdae296a46d626bfeaa5f868060ce
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-11-30 19:02:02 +0500

    In progress 30%

---
+++ lib/presentation/screens/feedback/feedback_screen.dart
@@ -0,0 +1,26 @@
+class FeedbackScreen extends StatelessWidget {
+  final String imagePath;
+  const FeedbackScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Photo Feedback")),
+      body: Column(
+        children: [
+          Image.file(File(imagePath)),
+          const SizedBox(height: 20),
+          const Text("Composition Score: 85%", style: TextStyle(fontSize: 20)),
+          const Text("Lighting Score: Good"),
+          const Text("Sharpness: OK"),
+          ElevatedButton(
+            child: const Text("Save"),
+            onPressed: () {
+              // save logic later
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
